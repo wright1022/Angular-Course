9910f6269fd8ca69d29774194642f1540c4505a4
wright1022 <MichaelFWright42@gmail.com>
2021-04-26 14:42:30 -0500

    update for video 18
    Added databinding to servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -20,6 +22,14 @@ export class ServersComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
