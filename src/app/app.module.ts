dcf8cc9f6b01b6ce241f6c0298d68218e1cc3dcd
wright1022 <MichaelFWright42@gmail.com>
2021-04-26 11:58:53 -0500

    Update for video 8
    changed to inline template for servers.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
   ],
   imports: [
     BrowserModule
